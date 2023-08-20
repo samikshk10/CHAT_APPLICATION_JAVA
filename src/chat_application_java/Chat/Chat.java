f0f1923fc6b2f9458347b018e71c26aec7131e2f
samik shk <samik.shakya8848@gmail.com>
2023-08-20 14:48:51 +0545

    chat:

---
+++ src/chat_application_java/Chat/Chat.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chat_application_java.Chat;
+
+/**
+ *
+ * @author Samik Shk
+ */
+public class Chat {
+    public static void main(String[] args)
+    {
+        System.out.println("hello world");
+    }
+    
+}
